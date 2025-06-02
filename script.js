318174d9471661997383d9546b95bab2de3a8226
abiyyurevanzha <abiyyudaffarevanzha@gmail.com>
2025-06-02 19:00:58 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const map = L.map('map').setView([-7.5, 110], 6);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+fetch('data/kavling.geojson')
+  .then(res => res.json())
+  .then(data => {
+    L.geoJSON(data, {
+      onEachFeature: function (feature, layer) {
+        layer.bindPopup(`
+          <strong>${feature.properties.nama}</strong><br/>
+          NIM: ${feature.properties.nim}<br/>
+          Wisuda: ${feature.properties.wisuda}
+        `);
+      },
+      style: {
+        color: "#0077b6",
+        weight: 2,
+        fillOpacity: 0.4
+      }
+    }).addTo(map);
+  });
